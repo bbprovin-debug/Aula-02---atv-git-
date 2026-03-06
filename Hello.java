9ed32d83ff7b2ad5a66e0a84484e89f5e0dd7c8b
Bprovin <brunabprovin@gmail.com>
2026-03-06 20:17:44 -0300

    Adicionado Hello

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void falar() {
+        System.out.println("Hallo Welt!");
     }
 
 }
